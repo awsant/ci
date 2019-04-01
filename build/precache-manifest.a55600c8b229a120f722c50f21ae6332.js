94a0738ff028b0a2a5142cf986e307900d1e16f6
Anton <puchkova@amazon.com>
2019-04-01 13:28:07 -0500

    first commit

---
+++ build/precache-manifest.a55600c8b229a120f722c50f21ae6332.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "7d75bc7dce55e57ba4c7",
+    "url": "/static/js/main.5f50fa4b.chunk.js"
+  },
+  {
+    "revision": "a539ea9d3b1acab9c730",
+    "url": "/static/js/2.948d4d77.chunk.js"
+  },
+  {
+    "revision": "7d75bc7dce55e57ba4c7",
+    "url": "/static/css/main.1eb9988f.chunk.css"
+  },
+  {
+    "revision": "7667b265eaf963e80f32181e4a717aa1",
+    "url": "/index.html"
+  }
+];
